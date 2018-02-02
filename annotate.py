580b0cf94c90d8bd2d96d3da9d4eeb9c3cc9361a
jrbelyeu <jrbelyeu@gmail.com>
2018-02-01 22:34:30 -0700

    modified operation option and added mode + stdev

--- annotate.py
+++ annotate.py
@@ -6,16 +6,18 @@ import argparse
 import os
 import datetime
 import cyvcf2
-import numpy
+import statistics
 
-def comma_list(string):
-   return string.split(',')
+def comma_list(in_str):
+   return list(filter(None ,in_str.split(',')))
 
 parser = argparse.ArgumentParser(description="creates an annotated variant file (vcf) with the results from PlotCritic scoring")
 parser.add_argument("-s", "--scores", help="file of results from PlotCritic scoring",required=True)
 parser.add_argument("-v", "--vcf", help="variant file to annotate",required=True)
 parser.add_argument("-a", "--annotated_outfile", help="name for new annotated variant file",required=True)
-parser.add_argument("-o", "--operation", help="summarizing operation for scores",required=False, choices=["min", "max", "mean", "median"])
+parser.add_argument("-o", "--operation", help="summarizing operation for scores. " + 
+        "Options are mean, median, standard deviation (stdev), mode, max, min",
+        required=False, choices=["mean", "median", "stdev", "mode", "max", "min"])
 parser.add_argument("-n", "--number_map", help="ordered list of number values for answers, used for summarizing results."+
         " Required if --operation is included. Example: if the curation answers in order are: 'yes', 'no', 'maybe' => 1,0,0.5 ", 
         type=comma_list, required=False)
@@ -40,6 +42,7 @@ with open(args.scores, 'r') as scores:
         else:
             if args.number_map and len(answers) != len(args.number_map):
                 print ("Error: count of curation answers does not equal count of number values in number_map")
+                print ("Curation answers: " + ", ".join(answers))
                 parser.print_help()
                 sys.exit(0)
             fields = line.split("\t")
@@ -66,7 +69,6 @@ for key in scored_variants:
                 latest_timestamp = entry[1]
         scored_variants[key]['score_fields'][answer] += 1
         scored_variants[key]['score_fields']['scorer_count'] += 1
-
 vcf = cyvcf2.VCF(os.path.expanduser(args.vcf))
 vcf.add_info_to_header({"ID": "SVPD", "Description": "Details of SV-plaudit scorer count and scores in the format COUNT|SCORE1,SCORE2,SCOREN. Answers the question: `" + question + "` Available answers were as follows: `" + "`; `".join(answers) + "`", "Type":'Character', 'Number':'1'})
 vcf.add_info_to_header({"ID": "SVP", "Description": "SV-plaudit curation score, the " + args.operation + " of scores for that entry where the values of the following curation answers: `" +  "`; `".join(answers) + "` are " + ",".join(args.number_map), "Type":'Float',  'Number':'1'})
@@ -88,8 +90,17 @@ for variant in vcf:
                 score_values = []
                 for i in range (len(score_counts)):
                     score_values += [float(args.number_map[i])] * int(score_counts[i])
-                curation_score = str(getattr(numpy,args.operation)(score_values))
-                variant.INFO['SVP'] = curation_score
+                curation_score = 0
+                try:
+                    if args.operation == "max" or args.operation == "min":
+                        curation_score = eval(args.operation)(score_values)
+                    else:
+                        curation_score = getattr(statistics,args.operation)(score_values)
+                except:
+                    print ("Warning: failed to perform specified operation on entry:")
+                    print (variant)
+                    continue
+                variant.INFO['SVP'] = str(curation_score)
             variant.INFO['SVPD'] = vcf_annotation
         writer.write_record(variant)
 writer.close()
